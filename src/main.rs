ac889a3a37c15e0e263dceb2f4f0529710e5b6b6
ProtasMV <160389679+ProtasMV@users.noreply.github.com>
2026-04-27 13:12:00 +0300

    1.1.0 | Code refactoring
        1. Refactoring main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,22 @@ use colored::*;
 use rand::RngExt;
 
 fn main() {
-    let mut user_input = String::new();
+    loop {
+        let mut user_input = String::new();
 
-    println!("{}", "Welcome to Duble my money game!".green().bold());
-    println!("1)Play, 2)Rules");
+        println!("{}", "Welcome to Duble my money game!".green().bold());
+        println!("1)Play, 2)Rules");
 
-    match io::stdin().read_line(&mut user_input) {
-        Ok(_) => print!(""),
-        Err(er) => println!("Program crash, error: {er}")
-    }
+        match io::stdin().read_line(&mut user_input) {
+            Ok(_) => print!(""),
+            Err(er) => println!("Program crash, error: {er}")
+        }
 
-    if user_input.trim() == "1" {
-        game();
-    } else if user_input.trim() == "2" {
-        description();
+        match &*user_input.trim().to_lowercase() {
+            "1"|"play" => {println!(); game(); break;}
+            "2"|"rules" => {println!(); description();}
+             _  => {println!("{}", "Try to write [1] or [2]".red()); println!(); continue;}
+        }
     }
 }
 
@@ -25,73 +27,64 @@ fn game() {
     let mut user_input = String::new();
     let mut money = 100;
 
-    println!("Your money: {money}");
-
+    println!("Money: {money}");
     loop {
-
-        if money <= 0 {
-            println!("{}", "I am sorry guy, but you didn't win anything".red());
-            println!(" "); 
-            
-            let user_continue = game_continue();
-            if user_continue == true {
-                money = 100;
-                continue;
-            } else if user_continue == false {
-                break;
-            }        
-        }
-
-        println!(" ");        
-        println!("Risk or safe?");
-        println!("tip: 1)Risk, 2)Safe");
-            
+        
+        println!("{}", "Risk or safe?".blue().bold());
+        println!("Tip: 1)Risk, 2)Safe");
+        
         user_input.clear();
         match io::stdin().read_line(&mut user_input) {
-            Ok(_) => print!(""),
-            Err(er) => println!("Program crash, error: {er}")
+            Ok(_) => print!(" "),
+            Err(er) => println!("Something went wrong, error: {er}")
         }
+        println!();
 
-        if user_input.trim() == "1" || user_input.trim().to_lowercase() == "risk" {
-            let lucky = rng.random_range(1..=2);
-            user_input.clear();
-            
-            if lucky == 1 {
-                user_input.clear();
-                money *=2;
+        match &*user_input.trim().to_lowercase() {
+            "1"|"risk" => {
+                let lucky = rng.random_bool(0.5);
+                
+                match lucky {
+                    true => {
+                        money *= 2;
 
-                println!("Congratulations! I double your money");
-                print!("{}", "Now, your money: ".green());
-                println!("{money}");
-                } else if lucky == 2 {
-                money = 0;
-                    
-                println!(" ");
-                println!("{}", "Bad News, I theft your money");
-                print!("{}", "Now, your money: ".red());
-                println!("{money}");
-                } else {
-                println!("Random error, random number: {lucky}");
-            }
-        } else if user_input.trim() == "2" || user_input.trim().to_lowercase() == "safe" {
-            user_input.clear();
-            match money {
-                0 => println!("{}", "I am sorry guy, but you didn't win anything".red().bold()),
-                100 => println!("{}", "Apparently, you've decided to give up the game.".blue().bold()),
-                _ => {print!("{}", "You win: ".green().bold()); print!("{money}"); println!("{}", "$".green().bold());}
-            }
+                        println!("{}", "Good news! I double your money".green().bold());
+                        println!("Now, your money: {money}$");
+                    },
+                    false => {
+                        money = 0;
 
-            let user_continue = game_continue();
-            if user_continue == true {
+                        println!("{}", "Bad news! I theft you money".red().bold());
+                        println!("Now, your money: {money}$");
+                        
+                        let user_continue = game_continue();
+                        match user_continue {
+                            true => {money = 100; continue},
+                            false => break
+                        }                    
+                    }
+                }   
+            },
+            "2"|"safe" => {
+                match money {
+                    100 => println!("{}", "Apparently you decided not to try your luck".purple().bold()),
+                     _  => {print!("{}", "Smart choise! You win: ".purple().bold()); print!("{money}"); println!("{}", "$".purple().bold())}
+                }
+
+                let user_continue = game_continue();
                 money = 100;
-                continue;
-            } else if user_continue == false {
-                break;
-            }
+                match user_continue {
+                    true => continue,
+                    false => break
+                }
 
-        } else {
-            println!("Input error, please try [1] or [2]"); user_input.clear(); continue;
-        }       
+            },
+            _ => {
+                println!("{}", "Try to write [1] or [2]".red());
+                println!();
+                continue;
+            }
+        }
     }
 }
 
@@ -105,31 +98,30 @@ you can take a risk and have a 50% chance of doubling the amount or losing every
 Focus on your own intuition, good luck!");
 
     println!(" ");
-    println!("{}", "Press anything key to continue".blue());
+    println!("{}", "Press anything key to continue".blue().bold());
 
     match io::stdin().read_line(&mut user_input) {
         Ok(_) => print!(" "),
         Err(er) => println!("Program crash, error: {er}")
     }
 
-    main();
 }
 
 fn game_continue() -> bool{
     let mut user_input = String::new();
 
     loop {
-        println!("You want play again? 1)Yes, 2)No");
+        println!("{}", "You want play again? 1)Yes, 2)No".blue());
                 
         match io::stdin().read_line(&mut user_input) {
             Ok(_) => print!(""),
             Err(er) => println!("Program crash, error: {er}")
         }
         
-        match user_input.trim() {
-            "1" => {user_input.clear(); break true},
-            "2" => {user_input.clear(); break false},
-            _  => {user_input.clear(); println!("Please write [1] or [2]"); println!(" "); continue}
+        match &*user_input.trim().to_lowercase() {
+            "1"|"yes" => {user_input.clear(); println!();break true},
+            "2"|"no" => {user_input.clear(); println!();break false},
+            _  => {user_input.clear(); println!("{}", "Please write [1] or [2]".red()); println!(" "); continue}
         };
     }
 }
